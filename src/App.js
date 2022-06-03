7f2f750f963d223198f8c1aa7b689d928cc30e3c
osigie <90554061+osigie@users.noreply.github.com>
2022-06-03 11:00:07 +0100

    added display message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import {useSelector} from "react-redux"
-import React, {useEffect } from "react"
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { uiActions } from "./components/store/ui-slice";
+import Notification from "./components/UI/Notification";
 
 function App() {
-  const cartIsVisible = useSelector((store)=>store.ui.cartIsVisible)
-  const cart = useSelector((store)=>store.cart)
+  const dispatch = useDispatch();
+  const cartIsVisible = useSelector((store) => store.ui.cartIsVisible);
+  const notification = useSelector((store) => store.ui.notification);
+  const cart = useSelector((store) => store.cart);
 
-  useEffect(()=>{
-fetch("https://redux-59ee1-default-rtdb.firebaseio.com/cart.json", {method: "PUT", body: JSON.stringify(cart)})
-  },[cart])
+  useEffect(() => {
+    const sendCardData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          title: "Sending....",
+          status: "pending",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://redux-59ee1-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart failed!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          title: "Success",
+          status: "Success",
+          message: "Send cart data successfully!",
+        })
+      );
+
+      const data = await response.json();
+    };
+
+    sendCardData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          title: "error",
+          status: "error",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart]);
 
 
-  
   return (
-    <Layout>
-      {cartIsVisible &&     <Cart />}
-  
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.name}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store/index";
+import store from "./components/store/index";
 import "./index.css";
 import App from "./App";
 
