37469446fb023cf63b3306f10ae29c98d1b91e80
osigie <90554061+osigie@users.noreply.github.com>
2022-06-03 05:14:47 +0100

    integrated firebase to save cart

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector} from "react-redux"
-
+import React, {useEffect } from "react"
 
 function App() {
   const cartIsVisible = useSelector((store)=>store.ui.cartIsVisible)
+  const cart = useSelector((store)=>store.cart)
+
+  useEffect(()=>{
+fetch("https://redux-59ee1-default-rtdb.firebaseio.com/cart.json", {method: "PUT", body: JSON.stringify(cart)})
+  },[cart])
+
+
+  
   return (
     <Layout>
       {cartIsVisible &&     <Cart />}
