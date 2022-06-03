dd1382925f10bf28680615fa3b49ca342d743936
osigie <90554061+osigie@users.noreply.github.com>
2022-06-03 04:25:18 +0100

    done with cart addition

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,22 @@
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
-
+import { useSelector } from 'react-redux';
 const Cart = (props) => {
+  const cart = useSelector((store)=>store.cart.items)
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cart.map((item,index)=>{
+          return (
+            <CartItem
+            item={{ title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price}}
+            key = {index}
+          />
+          )
+        })}
+       
       </ul>
     </Card>
   );
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,52 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+
+
+
+const dummyProduct = [
+ {
+   id:1,
+    title:'Automobile',
+  price:90,
+  description:'The first car i ever made!'},
+  {
+    id:2,
+     title:'Compression ignition',
+   price:10000,
+   description:'The second car i ever made!'},
+   {
+    id:3 ,
+     title:'Spark ignition Engine',
+   price:90,
+   description:'The third car i ever made!'},
+   {
+    id:4,
+     title:'Mechanical Governor',
+   price:4000,
+   description:'The fourth car i ever made!'},
+   
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+
+        {dummyProduct.map((item, index)=>{
+          return(
+            <ProductItem
+            title={item.title}
+            price={item.price}
+            description={item.description}
+            key= {index}
+            id = {item.id}
+          />
+          )
+
+        })}
+     
       </ul>
     </section>
   );
